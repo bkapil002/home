8154328da3fbfe7736460c3762089ec55ab678a2
bkapil002 <kapilabrahm@gmail.com>
2025-10-06 16:18:27 +0545

    hit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
 import Footer from "./components/Footer"
 import Home from "./components/Home"
 import Navebar from "./components/Navebar"
+import SamzaraDisclaimer from "./components/SamzaraDisclaimer";
+import SamzaraPrivacyPolicy from "./components/SamzaraPrivacyPolicy";
 import ScrollButton from "./components/ScrollButton"
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
 
 
   return (
     <>
+    <Router>
      <Navebar/>
-     <Home/>
+     <Routes>
+      
+     <Route path="/" element={<Home/>}  />
+      <Route path="/disclaimer" element={<SamzaraDisclaimer/>}/>
+      <Route path="/privacy-policy" element={<SamzaraPrivacyPolicy/>}/>
+     </Routes>
      <ScrollButton/> 
      <Footer/>
+     </Router>
     </>
   )
 }
